f2a3216b91c6b62e386da378d42547efa76c5679
Hendrik Ordoñez B <hendrikob.one@gmail.com>
2018-09-30 16:25:18 -0500

    Final Header and Promo.vue test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header msg="Welcome to Your Vue.js App"/>
+    <Promo></Promo>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Promo from './components/Promo.vue'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Promo
   }
 }
 </script>
